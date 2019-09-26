83a25c19b2cc1d3561a2c847292d4bc1b21be738
Alex Eyre <alex.eyre@cambridgeconsultants.com>
2019-09-26 14:24:49 +0100

    Add qutebrowser

---
+++ qutebrowser/config.py
@@ -0,0 +1,15 @@
+c.fonts.monospace = "FuraCode NF"
+c.tabs.position = "left"
+c.colors.tabs.bar.bg = "#2c2c2c"
+c.tabs.show = "multiple"
+c.tabs.last_close = "default-page"
+c.colors.statusbar.normal.bg = "#2c2c2c"
+
+
+c.url.searchengines = {"DEFAULT": "https://duckduckgo.com?q={}", "gg": "https://google.co.uk/search?query={}", "yt": "https://youtube.com/results?search_query={}", "scholar": "https://scholar.google.com/scholar?q={}", "wiki": "https://en.wikipedia.org/w/index.php?search={}"}
+
+c.content.headers.user_agent = "Chrome/76.0.3809.132"
+c.content.headers.accept_language = "en-US,en;q=0.5"
+c.content.headers.custom = {"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"}
+c.content.canvas_reading = False
+c.content.webgl = False
