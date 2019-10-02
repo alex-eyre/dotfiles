ef6393827631550578e36054d6210dff51977a30
Alex Eyre <alex.eyre@cambridgeconsultants.com>
2019-10-02 11:57:21 +0100

    Update qutebrowser

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -6,9 +6,15 @@ c.tabs.last_close = "default-page"
 c.colors.statusbar.normal.bg = "#2c2c2c"
 
 
-c.url.searchengines = {"DEFAULT": "https://duckduckgo.com?q={}", "gg": "https://google.co.uk/search?query={}", "yt": "https://youtube.com/results?search_query={}", "scholar": "https://scholar.google.com/scholar?q={}", "wiki": "https://en.wikipedia.org/w/index.php?search={}"}
+c.url.searchengines = {"DEFAULT": "https://duckduckgo.com?q={}",
+                       "gg": "https://google.co.uk/search?query={}",
+                       "yt": "https://youtube.com/results?search_query={}",
+                       "scholar": "https://scholar.google.com/scholar?q={}",
+                       "wiki": "https://en.wikipedia.org/w/index.php?search={}",
+                       "amaz": "https://amazon.co.uk/s?k={}",
+                       "note": "file:///C:/Users/ase/notes/{}.html"}
 
-c.content.headers.user_agent = "Chrome/76.0.3809.132"
+c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
 c.content.headers.accept_language = "en-US,en;q=0.5"
 c.content.headers.custom = {"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"}
 c.content.canvas_reading = False
---
+++ qutebrowser/greasemonkey/notion-inline-latex.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name            Notion Inline LaTeX
+// @namespace       https://github.com/Penguinlay/notion-inline-latex
+// @match           https://www.notion.so/*
+// @version         0.5
+// @description     User Script for Inline LaTeX Rendering in notion.so
+// @require         https://cdn.jsdelivr.net/npm/katex@0.10.2/dist/katex.min.js
+// @require         https://cdn.jsdelivr.net/npm/katex@0.10.2/dist/contrib/auto-render.min.js
+// @grant           GM_addStyle
+// ==/UserScript==
+
+// Note:
+// Script does not change the content.
+// It merely changes the display after the contents are loaded.
+// Need at least one math block present on the page for the script to work properly.
+// Small value for startUpWaitTime and keyUpWaitTime can make the script load improperly.
+// Add delimiters as desired though delimiters other than default one have not been tested yet.
+
+// Acknowledgement
+// This script is inspired by the project by Evert Heylen.
+// https://github.com/evertheylen/notion-inline-math
+
+// right-padding for inline math mode
+GM_addStyle(`
+.katex {
+    padding-right: 0 !important;
+}
+`);
+
+// declare/init vars
+let timer; // timer identifier
+let startUpWaitTime = 3000; // ms after startup (adjust as needed)
+let keyUpWaitTime = 2000; // ms after keyup   (adjust as needed)
+
+// render inline LaTeX
+function renderInlineLaTeX() {
+  renderMathInElement(document.body, {
+    delimiters: [
+      // LaTeX delimiters (uncomment/add as needed)
+      // { left: "$$" , right: "$$" , display: true  },
+      // { left: "\\[", right: "\\]", display: true  },
+      // { left: "\\(", right: "\\)", display: false },
+      { left: "$", right: "$", display: false }
+    ]
+  });
+  console.log("Inline LaTeX is rendered.");
+}
+
+// on startup...
+console.log("Rendering inline LaTeX on startup...");
+clearTimeout(timer);
+timer = setTimeout(renderInlineLaTeX, startUpWaitTime);
+
+// on keyup...
+document.addEventListener("keyup", () => {
+  console.log("Rendering inline LaTeX on keyup...");
+  clearTimeout(timer);
+  timer = setTimeout(renderInlineLaTeX, keyUpWaitTime);
+});
