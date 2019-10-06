938057643212d8e097366cef3888ccab9886bd32
Alex Eyre <alex.eyre@cambridgeconsultants.com>
2019-10-06 19:47:58 +0100

    Emacs

--- emacs/snippets.el
+++ emacs/snippets.el
@@ -1,4 +1,7 @@
+(use-package yasnippet-snippets)
+(straight-use-package
+ '(yasnippets-orgmode :type git :host github :repo "yyr/yasnippets-orgmode"))
+
 (use-package yasnippet
   :demand t
   :init(yas-global-mode 1))
-(use-package yasnippet-snippets)
