28015326579854ebd50948db1e3264dd0cdb50d8
Alex Eyre <alexeeyre@gmail.com>
2019-10-24 15:33:52 +0100

    Add configure arg script

---
+++ emacs/configure.sh
@@ -0,0 +1,1 @@
+./configure --enable-link-time-optimization --with-x --with-x-toolkit=lucid --with-file-notification=inotify --without-toolkit-scroll-bars --without-pop --with-mailutils --with-cairo
